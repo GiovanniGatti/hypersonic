cb475b1ffab8a546ef08de43d9417e1445dd6b3b
Giovanni Gatti <giovanni.gatti.pinheiro@gmail.com>
2016-09-24 19:19:48 +0200

    Hypersonic setting up

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -4,7 +4,8 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Scanner;
-import java.util.function.IntSupplier;
+
+import jdk.nashorn.internal.ir.annotations.Immutable;
 
 public final class Player {
 
@@ -12,8 +13,8 @@ public final class Player {
 
         Scanner in = new Scanner(System.in);
 
-        Repository repo = null;
-        AI ai = null;
+        RepositoryUpdater repo = new InputRepository(in);
+        AI ai = new StateMachineAI(repo);
 
         while (true) {
             ai.updateRepository();
@@ -24,17 +25,103 @@ public final class Player {
         }
     }
 
+    public static class StateMachineAI extends AI {
+
+        public StateMachineAI(RepositoryUpdater updater) {
+            super(updater);
+        }
+
+        @Override
+        public Action[] play() {
+            return new Action[] { Action.bomb(6, 5) };
+        }
+    }
+
+    public static class InputRepository implements RepositoryUpdater {
+
+        private final Scanner in;
+
+        private final int width;
+        private final int height;
+        private final int myId;
+
+        InputRepository(Scanner in) {
+            this.in = in;
+            this.width = in.nextInt();
+            this.height = in.nextInt();
+            this.myId = in.nextInt();
+            in.nextLine();
+        }
+
+        @Override
+        public void update() {
+
+            for (int i = 0; i < height; i++) {
+                String row = in.nextLine();
+            }
+
+            int entities = in.nextInt();
+            for (int i = 0; i < entities; i++) {
+                int entityType = in.nextInt();
+                int owner = in.nextInt();
+                int x = in.nextInt();
+                int y = in.nextInt();
+                int param1 = in.nextInt();
+                int param2 = in.nextInt();
+            }
+
+            in.nextLine();
+        }
+
+        public int getWidth() {
+            return width;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+
+        public int getMyId() {
+            return myId;
+        }
+    }
+
+    public enum ActionType {
+        MOVE, BOMB
+    }
+
     /**
      * Represents an action that can be taken
      */
+    @Immutable
     public static class Action {
 
-        public Action() {
-            // TODO: implement what action is
+        private final ActionType type;
+        private final int x;
+        private final int y;
+        private final String message;
+
+        public static Action move(int x, int y) {
+            return new Action(ActionType.MOVE, x, y);
+        }
+
+        public static Action bomb(int x, int y) {
+            return new Action(ActionType.BOMB, x, y);
+        }
+
+        public Action(ActionType type, int x, int y) {
+            this(type, x, y, "");
+        }
+
+        public Action(ActionType type, int x, int y, String message) {
+            this.type = type;
+            this.x = x;
+            this.y = y;
+            this.message = message;
         }
 
         public String asString() {
-            return "";
+            return type.name() + " " + x + " " + y + " " + message;
         }
     }
 
@@ -90,24 +177,6 @@ public final class Player {
         @Override
         public final int hashCode() {
             return Objects.hash(conf, getClass());
-        }
-    }
-
-    public static abstract class Repository {
-
-        private final IntSupplier inputSupplier;
-
-        protected Repository(IntSupplier inputSupplier) {
-            this.inputSupplier = inputSupplier;
-        }
-
-        /**
-         * Reads and parse input stream.
-         */
-        public abstract void update();
-
-        protected int readInput() {
-            return inputSupplier.getAsInt();
         }
     }
 
