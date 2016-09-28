4f75b203dc566ac0fc84861cd86422c54654d3db
Giovanni Gatti <giovanni.gatti.pinheiro@gmail.com>
2016-09-28 23:30:04 +0200

    WIP: Developping a Chromossome

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Queue;
+import java.util.Random;
 import java.util.Scanner;
 
 import jdk.nashorn.internal.ir.annotations.Immutable;
@@ -42,6 +43,252 @@ public final class Player {
         public Action[] play() {
 
             return new Action[] { Action.move(0, 0) };
+        }
+    }
+
+    Chromosome find(int movements, int popSize, int generations) {
+
+        // Create the pool
+        List<Chromosome> pool = new ArrayList<>(popSize);
+        List<Chromosome> newPool = new ArrayList<>(popSize);
+
+        // Generate unique chromosomes in the pool
+        for (int i = 0; i < popSize; i++) {
+            Chromosome chromosome = new Chromosome(movements, map, totalScore, busters);
+            chromosome.score();
+            pool.add(chromosome);
+        }
+
+        // Loop until solution is found
+        for (int generation = 0; generation < generations; generation++) {
+            // Clear the new pool
+            newPool.clear();
+
+            // Loop until the pool has been processed
+            for (int x = pool.size() - 1; x >= 0; x -= 2) {
+                // Select two members
+                Chromosome n1 = selectMember(pool);
+                Chromosome n2 = selectMember(pool);
+
+                // Cross over and mutate
+                n1.crossOver(n2);
+                n1.mutate();
+                n2.mutate();
+
+                // score new nodes
+                n1.score();
+                n2.score();
+
+                // Add to the new pool
+                newPool.add(n1);
+                newPool.add(n2);
+            }
+
+            // Add the newPool back to the old pool
+            pool.addAll(newPool);
+
+            // double tot = 0.0;
+            // for (int x = newPool.size() - 1; x >= 0; x--) {
+            // double score = (newPool.get(x)).score;
+            // tot += score;
+            // }
+            // System.out.println("generation " + generation + ": " + tot);
+        }
+
+        double maxScore = Double.NEGATIVE_INFINITY;
+        Chromosome best = null;
+        for (Chromosome chromosome : newPool) {
+            if (chromosome.score > maxScore) {
+                maxScore = chromosome.score;
+                best = chromosome;
+            }
+        }
+
+        return best;
+    }
+
+    private Chromosome selectMember(List<Chromosome> l) {
+
+        // Get the total fitness
+        double tot = 0.0;
+        for (int x = l.size() - 1; x >= 0; x--) {
+            double score = (l.get(x)).score;
+            tot += score;
+        }
+        double slice = tot * random.nextDouble();
+
+        // Loop to find the node
+        double ttot = 0.0;
+        for (int x = l.size() - 1; x >= 0; x--) {
+            Chromosome node = l.get(x);
+            ttot += node.score;
+            if (ttot >= slice) {
+                l.remove(x);
+                return node;
+            }
+        }
+
+        return l.remove(l.size() - 1);
+    }
+
+    private static class Chromosome {
+        private final double crossoverRate;
+        private final double mutationRate;
+        private final double[][] map;
+        private final double mapTotalScore;
+        private final Buster[] busters;
+        private final Random random;
+
+        private int[][] genes;
+        private double score;
+
+        Chromosome(int numberOfMovements,
+                double crossoverRate,
+                double mutationRate,
+                double[][] map,
+                double mapTotalScore,
+                Buster... busters) {
+
+            this.crossoverRate = crossoverRate;
+            this.mutationRate = mutationRate;
+            this.map = map;
+            this.mapTotalScore = mapTotalScore;
+            this.busters = busters;
+            random = new Random();
+            genes = new int[numberOfMovements * busters.length][2];
+            for (int[] gene : genes) {
+                double nextDouble = random.nextDouble();
+                if (nextDouble < .15) {
+                    gene[0] = random.nextInt(5500);
+                    gene[1] = random.nextInt(3500) + 5500;
+                } else if (nextDouble < .30) {
+                    gene[0] = 10500 + random.nextInt(5500);
+                    gene[1] = random.nextInt(5500);
+                } else {
+                    gene[0] = random.nextInt(16000);
+                    gene[1] = random.nextInt(9000);
+                }
+            }
+
+            score = 0.0;
+        }
+
+        Chromosome(int numberOfMovements, double[][] map, double mapTotalScore, Buster... busters) {
+            this(numberOfMovements, .7, .001, map, mapTotalScore, busters);
+        }
+
+        void score() {
+            boolean[][] mask = new boolean[map.length][map[0].length];
+            double score = 0.0;
+
+            int[][] bustersPositions = new int[busters.length][2];
+            for (int i = 0; i < busters.length; i++) {
+                bustersPositions[i][0] = busters[i].getX();
+                bustersPositions[i][1] = busters[i].getY();
+            }
+
+            for (int g = 0; g < genes.length; g++) {
+
+                int xi = bustersPositions[g % busters.length][0];
+                int yi = bustersPositions[g % busters.length][1];
+
+                int xt = genes[g][0];
+                int yt = genes[g][1];
+
+                int[] move = move(xi, yi, xt, yt);
+                int x = move[0];
+                int y = move[1];
+                bustersPositions[g % busters.length][0] = x;
+                bustersPositions[g % busters.length][1] = y;
+
+                int upperX = (x - FOW_RANGE) / MAP_RESOLUTION;
+                int upperY = (y - FOW_RANGE) / MAP_RESOLUTION;
+
+                int bottomX = (x + FOW_RANGE) / MAP_RESOLUTION;
+                int bottomY = (y + FOW_RANGE) / MAP_RESOLUTION;
+
+                // map bord constraints
+                if (upperX < 0) {
+                    upperX = 0;
+                }
+
+                if (upperY < 0) {
+                    upperY = 0;
+                }
+
+                if (bottomX > map.length) {
+                    bottomX = map.length;
+                }
+
+                if (bottomY > map[0].length) {
+                    bottomY = map[0].length;
+                }
+
+                for (int j = upperX; j < bottomX; j++) {
+                    int blockX = j * MAP_RESOLUTION;
+                    for (int i = upperY; i < bottomY; i++) {
+
+                        if (map[j][i] == 0.0 || mask[j][i]) {
+                            continue;
+                        }
+
+                        int blockY = i * MAP_RESOLUTION;
+
+                        int dx = blockX - x;
+                        int dy = blockY - y;
+
+                        if (blockX >= x && j < blockY) {
+                            // upper right corner
+                            dx = (dx + MAP_RESOLUTION);
+                        } else if (blockX >= x && j >= blockY) {
+                            // lower right corner
+                            dx = (dx + MAP_RESOLUTION);
+                            dy = (dy + MAP_RESOLUTION);
+                        } else if (blockX <= x && j >= blockY) {
+                            // lower left corner
+                            dy = (dy + MAP_RESOLUTION);
+                        }
+
+                        if (dx * dx + dy * dy <= SQUARE_FOW_RANGE) {
+                            mask[j][i] = true;
+                            score += map[j][i];
+                        }
+                    }
+                }
+            }
+
+            this.score = 1.0 / (mapTotalScore - score);
+        }
+
+        void crossOver(Chromosome another) {
+            if (random.nextDouble() < crossoverRate) {
+                int randomGene = random.nextInt(genes.length);
+
+                int[][] child1 = new int[genes.length][];
+                int[][] child2 = new int[genes.length][];
+
+                for (int j = 0; j < randomGene; j++) {
+                    child1[j] = genes[j];
+                    child2[j] = another.genes[j];
+                }
+
+                for (int j = randomGene; j < genes.length; j++) {
+                    child1[j] = another.genes[j];
+                    child2[j] = genes[j];
+                }
+
+                this.genes = child1;
+                another.genes = child2;
+            }
+        }
+
+        void mutate() {
+            for (int[] gene : genes) {
+                if (random.nextDouble() <= mutationRate) {
+                    gene[0] = random.nextInt(16000);
+                    gene[1] = random.nextInt(9000);
+                }
+            }
         }
     }
 
